5f3d7bd86f4c20be5b179f91570c0571118cb19d
菊池響 <h.kikuchi.sys22@morijyobi.ac.jp>
2023-11-29 14:59:22 +0900

    課題1-5

--- scripts/my_scene.js
+++ scripts/my_scene.js
@@ -8,6 +8,7 @@ class MyScene extends Phaser.Scene {
          // 画像の読み込み(使用する時の名前, パス)
         this.load.image('sky', 'assets/background.png');
         this.load.image('taro', 'assets/taro.png');
+        this.load.image('jiro', 'assets/jiro.png');
     }
 
 
@@ -15,22 +16,61 @@ class MyScene extends Phaser.Scene {
     create() {
          // 単体画像をシーンに追加(X座標,Y座標,画像名)
         this.add.image(400, 300, 'sky');
+        // const player = this.physics.add.sprite(400, 300, 'taro');
+        // this.player = player
+        // this.player.angle = 0;
+        // this.player_direction = 1;
         const player = this.physics.add.sprite(400, 300, 'taro');
         this.player = player
-        this.player.angle = 0;
-        this.player_direction = 1;
+        const player1 = this.physics.add.sprite(350, 300, 'jiro');
+        this.player1 = player1
+    }
+
+    arrow_move(cursors, object){
+    
+        if(cursors.left.isDown){
+            console.log("Left");
+            object.setVelocityX(-200);
+    
+    
+        }else if(cursors.right.isDown){
+            console.log("Right!!");
+            object.setVelocityX(200);
+    
+        }else{
+            object.setVelocity(0,0);
+        }
+    }
+
+    arrow_move2(cursors, object){
+    
+        if(cursors.left.isDown){
+            console.log("Left");
+            object.setVelocityX(200);
+    
+    
+        }else if(cursors.right.isDown){
+            console.log("Right!!");
+            object.setVelocityX(-200);
+    
+        }else{
+            object.setVelocity(0,0);
+        }
     }
     
   // 毎フレーム実行される繰り返し処理
     update() {
-        if (this.player.x >= D_WIDTH -230 || this.player.y >= D_HEIGHT) this.player_direction = -1;
-        if (this.player.x <= 0 || this.player.y <= 0) this.player_direction = 1;
+        // if (this.player.x >= D_WIDTH -230 || this.player.y >= D_HEIGHT) this.player_direction = -1;
+        // if (this.player.x <= 0 || this.player.y <= 0) this.player_direction = 1;
 
-        if(this.player_direction == 1){
-            this.player.setVelocity(100, -100);
-            this.player.angle += 5;
-            this.player.setAngle( this.player.angle );
-        }
+        // if(this.player_direction == 1){
+        //     this.player.setVelocity(100, -100);
+        //     this.player.angle += 5;
+        //     this.player.setAngle( this.player.angle );
+        // }
+        let cursors = this.input.keyboard.createCursorKeys();
+        this.arrow_move(cursors, this.player);
+        this.arrow_move2(cursors, this.player1);
     }
 
 }
